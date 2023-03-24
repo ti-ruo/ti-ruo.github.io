efa4316992f9283c986e3ed3779529172f4713e5
Tiruo Yan <yantiruo@gmail.com>
2023-03-24 16:26:22 -0700

    change to hash router

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom'; // allow us to use the router method
+import { BrowserRouter, HashRouter } from 'react-router-dom'; // allow us to use the router method
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <BrowserRouter>
+  
     <React.StrictMode>
-      <App />
+      <HashRouter>
+        <App />
+      </HashRouter>
     </React.StrictMode>
-  </BrowserRouter>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
