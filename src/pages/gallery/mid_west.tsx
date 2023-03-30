9d2b6fcf7ccf547cfe0361fbbb23626e544895a5
Tiruo Yan <yantiruo@gmail.com>
2023-03-30 13:01:11 -0700

    more pics

--- src/pages/gallery/mid_west.tsx
+++ src/pages/gallery/mid_west.tsx
@@ -2,6 +2,21 @@ import React from 'react';
 import PhotoViewer from './image_list';
 // photos
 import mn_voyagers_0 from '../../images/mid_west/mn_voyagers_0.jpg'
+import az_hoover_0 from '../../images/mid_west//az_hoover_0.jpg'
+import az_hoover_1 from '../../images/mid_west//az_hoover_1.jpg'
+import lv_0 from '../../images/mid_west/lv_0.jpg'
+import bryce_2 from '../../images/mid_west/bryce_2.jpg'
+import bryce_1 from '../../images/mid_west/bryce_1.jpg'
+import bryce_0 from '../../images/mid_west/bryce_0.jpg'
+import lv_1 from '../../images/mid_west/lv_1.jpg'
+import mn_voyagers_1 from '../../images/mid_west/mn_voyagers_1.jpg'
+import mn_voyagers_2 from '../../images/mid_west/mn_voyagers_2.jpg'
+import mn_voyagers_3 from '../../images/mid_west/mn_voyagers_3.jpg'
+import ut_capital from '../../images/mid_west/ut_capital.jpg'
+import ut_first_kfc from '../../images/mid_west/ut_first_kfc.jpg'
+import ut_salt_lake_0 from '../../images/mid_west/ut_salt_lake_0.jpg'
+import ut_salt_lake_1 from '../../images/mid_west/ut_salt_lake_1.jpg'
+import wy_street from '../../images/mid_west/wy_street.jpeg'
 
 import { Button } from '@mui/material';
 interface Image {
@@ -14,6 +29,66 @@ const images: Image[] = [
     {
         src: mn_voyagers_0,
         alt: 'Nighty Sky and the Milky Way @Voyageurs National Park, MN'
+    },
+    {
+        src: mn_voyagers_1,
+        alt: 'Houses by Lakes @Voyageurs National Park, MN'
+    },
+    {
+        src: mn_voyagers_2,
+        alt: 'Snow on Pine @Voyageurs National Park, MN'
+    },
+    {
+        src: mn_voyagers_3,
+        alt: 'The Harbor @Voyageurs National Park, MN'
+    },
+    {
+        src: az_hoover_0,
+        alt: 'Hoover Dam Towers @Clark County, Nevada'
+    },
+    {
+        src: az_hoover_1,
+        alt: 'An Overview @Clark County, Nevada'
+    },
+    {
+        src: lv_0,
+        alt: 'The Loop @Las Vegas, NV'
+    },
+    {
+        src: lv_1,
+        alt: 'View on air @Las Vegas, NV'
+    },
+    {
+        src: bryce_0,
+        alt: 'Colours @Dixie National Forest'
+    },
+    {
+        src: bryce_1,
+        alt: '@Bryce Canyon National Park'
+    },
+    {
+        src: bryce_2,
+        alt: '@Bryce Canyon National Park'
+    },
+    {
+        src: ut_capital,
+        alt: 'Capital of Utah View @Salt Lake City, UT'
+    },
+    {
+        src: ut_first_kfc,
+        alt: 'Worlds first KFC @Salt Lake City, UT'
+    },
+    {
+        src: ut_salt_lake_0,
+        alt: 'Big Salt Lake @Salt Lake City, UT'
+    },
+    {
+        src: ut_salt_lake_1,
+        alt: 'Big Salt Lake @Salt Lake City, UT'
+    },
+    {
+        src: wy_street,
+        alt: 'On the road @Medicine Bow-Routt National Forest'
     },
 ];
 
--- src/pages/gallery/national_parks.tsx
+++ src/pages/gallery/national_parks.tsx
@@ -2,6 +2,18 @@ import React from 'react';
 import PhotoViewer from './image_list';
 // photos
 import mn_voyagers_0 from '../../images/mid_west/mn_voyagers_0.jpg'
+import mn_voyagers_1 from '../../images/mid_west/mn_voyagers_1.jpg'
+import mn_voyagers_2 from '../../images/mid_west/mn_voyagers_2.jpg'
+import mn_voyagers_3 from '../../images/mid_west/mn_voyagers_3.jpg'
+import bryce_2 from '../../images/mid_west/bryce_2.jpg'
+import bryce_1 from '../../images/mid_west/bryce_1.jpg'
+import bryce_0 from '../../images/mid_west/bryce_0.jpg'
+import death_valley_0 from '../../images/cali/death_valley_0.jpg'
+import death_valley_1 from '../../images/cali/death_valley_1.jpg'
+import death_valley_2 from '../../images/cali/death_valley_2.jpg'
+import pinnacles_0 from '../../images/cali/pinnacles_0.jpg'
+import pinnacles_1 from '../../images/cali/pinnacles_1.jpg'
+import pinnacles_2 from '../../images/cali/pinnacles_2.jpg'
 
 import { Button } from '@mui/material';
 interface Image {
@@ -14,6 +26,54 @@ const images: Image[] = [
     {
         src: mn_voyagers_0,
         alt: 'Nighty Sky and the Milky Way @Voyageurs National Park, MN'
+    },
+    {
+        src: mn_voyagers_1,
+        alt: 'Houses by Lakes @Voyageurs National Park, MN'
+    },
+    {
+        src: mn_voyagers_2,
+        alt: 'Snow on Pine @Voyageurs National Park, MN'
+    },
+    {
+        src: mn_voyagers_3,
+        alt: 'The Harbor @Voyageurs National Park, MN'
+    },
+    {
+        src: death_valley_0,
+        alt: 'A van driving through @Death Valley National Park'
+    },
+    {
+        src: death_valley_1,
+        alt: 'Dancing people when star gazing @Death Valley National Park'
+    },
+    {
+        src: death_valley_2,
+        alt: 'Small Mountains @Death Valley National Park'
+    },
+    {
+        src: pinnacles_0,
+        alt: 'Mountains in B/W @Pinnacles National Park'
+    },
+    {
+        src: pinnacles_1,
+        alt: 'A beautiful peek @Pinnacles National Park'
+    },
+    {
+        src: pinnacles_2,
+        alt: 'Death Stranding Scene @Pinnacles National Park'
+    },
+    {
+        src: bryce_0,
+        alt: 'Colours @Dixie National Forest'
+    },
+    {
+        src: bryce_1,
+        alt: '@Bryce Canyon National Park'
+    },
+    {
+        src: bryce_2,
+        alt: '@Bryce Canyon National Park'
     },
 ];
 
